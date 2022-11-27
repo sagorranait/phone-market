1fd7072af419353c0f19a5978cb88c8f2bad6f9c
Sagor Rana <sagorranait@gmail.com>
2022-11-28 00:54:46 +0600

    Showing the advertisement product is done.

--- src/components/Advertised.js
+++ src/components/Advertised.js
@@ -1,24 +1,22 @@
 import { Col, Container, Row } from 'react-bootstrap'
 import '../styles/Advertised.css'
+import Loading from './Loading'
 import ProductItem from './ProductItem'
 
-function Advertised() {
+function Advertised({loading, products}) {
    
   return (
     <section className='advertised-area'>
       <Container>
          <h2>Advertisement</h2>
-         <Row>
-            <Col sm={12} md={6} lg={4} xl={4} xxl={4}>
-               <ProductItem/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={4} xxl={4}>
-               <ProductItem/>
-            </Col>
-            <Col sm={12} md={6} lg={4} xl={4} xxl={4}>
-               <ProductItem/>
-            </Col>
+         {loading ? <Loading/> : 
+         <Row className={`${products.length === 1 || products.length === 2 ? 'justify-content-center' : ''}`}>
+            {products?.map(product => <Col key={product._id} sm={12} md={6} lg={4} xl={4} xxl={4}>
+               <ProductItem product={product}/>
+            </Col>)}
+            
          </Row>
+         }
       </Container>
     </section>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import Advertised from "../components/Advertised"
 import AppDownload from "../components/AppDownload"
 import Banner from "../components/Banner"
@@ -5,10 +7,25 @@ import Categories from "../components/Categories"
 import Services from "../components/Services"
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [advertiseProduct, setAdvertiseProduct] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('http://localhost:5000/product/advertised')
+    .then(res =>{ 
+      setAdvertiseProduct(res.data);
+      setLoading(false);
+    })
+    .catch(error => console.log(error));
+  }, []) ;
+
+  console.log(advertiseProduct);
+
   return (
     <div className='home-section'>
       <Banner/>
-      <Advertised/>
+      {advertiseProduct.length !== 0 ? <Advertised loading={loading} products={advertiseProduct}/> : null}
       <Categories/>
       <AppDownload/>
       <Services/>
