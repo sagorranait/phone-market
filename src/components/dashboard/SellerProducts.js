3c2a73c77b452c6ccc67c35dbdc9b359f3a48f0d
Sagor Rana <sagorranait@gmail.com>
2022-11-28 21:27:03 +0600

    Functionality of (Getting seller product, Deleting product, and Make Advertised) are done.

--- src/components/dashboard/SellerProducts.js
+++ src/components/dashboard/SellerProducts.js
@@ -1,12 +1,88 @@
-import '../../styles/dashboard/SellerProducts.css';
+import { useContext, useEffect, useState } from 'react';
 import SingleSellerProduct from "./SingleSellerProduct";
+import { StateContext } from '../../StateProvider';
+import '../../styles/dashboard/SellerProducts.css';
+import toast from 'react-hot-toast';
+import Loading from '../Loading';
 
 function SellerProducts() {
+  const { user } = useContext(StateContext);
+  const [sellerProduct, setSellerProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`http://localhost:5000/sellerproduct?email=${user?.email}`, {
+      headers: {
+          authorization: `Bearer ${localStorage.getItem('access-token')}`
+      }
+  })
+    .then(res => res.json())
+    .then(data => {
+      setSellerProduct(data)
+      setLoading(false);
+    })
+    .catch(error => console.log(error))
+  }, [user?.email]);
+
+  const advertisedHandler = (id) => {
+    const advertised = {
+      advertised: true,
+    }
+
+    fetch(`http://localhost:5000/sellerproduct/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(advertised)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          fetch(`http://localhost:5000/sellerproduct?email=${user?.email}`, {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('access-token')}`
+            }
+        })
+          .then(res => res.json())
+          .then(data => {
+            setSellerProduct(data)
+          })
+          .catch(error => console.log(error))
+          toast.success('Successfully Product Advertised. Check Home page !!');
+        }
+      })
+      .catch(error => console.log(error));
+  }
+
+ const deleteProductHandler = (id) => {
+  const sureDelete = window.confirm("Are you sure to delete!");
+  if (sureDelete) {
+    fetch(`http://localhost:5000/product/${id}`, {
+        method: 'DELETE',
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.deletedCount > 0) {
+          toast.success('Deleted Successfully.');
+          const remaining = sellerProduct.filter(odr => odr._id !== id);
+          setSellerProduct(remaining);
+        }
+    })
+    .catch((error)=> console.log(error));
+  }
+ }
+
   return (
     <div className='seller-products'>
       <div className='products'>
-         <SingleSellerProduct/>
-         <SingleSellerProduct/>
+         {sellerProduct?.length === 0 ? <p className='text-center pt-5'>No product is available!</p> :
+          <>
+            {loading ? <Loading/> : sellerProduct.map(product => <SingleSellerProduct key={product._id} product={product} advertisedHandler={advertisedHandler} deleteProductHandler={deleteProductHandler} />)}
+          </>
+          }
       </div>
     </div>
   )
--- src/components/dashboard/SingleSellerProduct.js
+++ src/components/dashboard/SingleSellerProduct.js
@@ -2,30 +2,35 @@ import Verified from '../../assets/verified.png';
 import '../../styles/dashboard/SingleSellerProduct.css';
 import ItemTemplate from "./ItemTemplate";
 
-function SingleSellerProduct() {
+function SingleSellerProduct({product, advertisedHandler, deleteProductHandler}) {
+   const {_id, used_year, title, saler_verified, saler_name, original_price, resale_price, post_date, phone_number, location, img_url, description, condition, advertised} = product;
+
   return (
    <ItemTemplate>
       <div className='product-image'>
-         <img src={'https://imgs.search.brave.com/Bd6z-QBHbK_pWsG9FEdRYBZXLa4Xh2ki_VnTSr0NtT4/rs:fit:493:506:1/g:ce/aHR0cHM6Ly9mcmVl/Ymllc2Nsb3VkLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/Mi8wMi9TYW1zdW5n/LUdhbGF4eS1TMjIt/VWx0cmEtOC5wbmc'} alt="product-title" />
+         <img src={img_url} alt={title} />
       </div>
       <div className='product-info'>
-         <h2>SAMSUNG Galaxy S22 Ultra</h2>
-         <h4>Price: <del>1199.99</del> <span>349.38</span></h4>
-         <p>128GB, 8K Camera & Video, Brightest Display Screen...</p>
+         <h2>{title}</h2>
+         <h4>Price: <del>{original_price}</del> <span>{resale_price}</span></h4>
+         <p>{description}</p>
       </div>
       <div className='product-status'>
-         <p>Condition: <span>good</span></p>
-         <p>Used: <span>1 year</span></p>
+         <p>Condition: <span>{condition}</span></p>
+         <p>Used: <span>{used_year} year</span></p>
       </div>
       <div className='product-seller'>
-         <p>Rakib Hasan <span><img src={Verified} alt="Verified" /></span></p>
-         <p>Call: 01812345698</p>
-         <p>Area: <span>Dhaka</span></p>
-         <p>Date: <span>2022-11-26</span></p>
+         <p>{saler_name} {saler_verified && <span><img src={Verified} alt="Verified" /></span>}</p>
+         <p>Call: {phone_number}</p>
+         <p>Area: <span>{location}</span></p>
+         <p>Date: <span>{post_date.slice(0, 10)}</span></p>
       </div>
       <div className='product-action product-advertise'>
-         <button className="phoneMarket-btn">Advertise</button>
-         <button className="phoneMarket-btn">Delete</button>
+         {advertised ? 
+            <button className="phoneMarket-btn advertised">Advertised</button> : 
+            <button className="phoneMarket-btn" onClick={()=>advertisedHandler(_id)}>Give Advertise</button>
+         }
+         <button className="phoneMarket-btn" onClick={()=>deleteProductHandler(_id)}>Delete</button>
       </div>
    </ItemTemplate>
   )
