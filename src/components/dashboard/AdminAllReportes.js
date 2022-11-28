896da9b7e5a0a37a2f602ad451ebdb04fa2df227
Sagor Rana <sagorranait@gmail.com>
2022-11-28 19:58:03 +0600

    Functionality of (All Reporte for Admin) is done.

--- src/components/dashboard/AdminAllReportes.js
+++ src/components/dashboard/AdminAllReportes.js
@@ -1,11 +1,60 @@
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import Loading from '../Loading';
 import SingleAdminReporte from './SingleAdminReporte'
 import TableHead from './TableHead'
 
 function AdminAllReportes() {
+  const [allReportes, setAllReportes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('http://localhost:5000/allReporte')
+    .then(res => res.json())
+    .then(data => {
+      setAllReportes(data)
+      setLoading(false);
+    })
+    .catch(error => console.log(error))
+  }, []);
+
+  const deleteReportedHandler = (id, proId) => {
+    const sureDelete = window.confirm("Are you sure to delete!");
+    if (sureDelete) {
+      fetch(`http://localhost:5000/reported/${id}`, {
+          method: 'DELETE',
+      })
+      .then(res => res.json())
+      .then(data => {
+          if (data.deletedCount > 0) {
+            toast.success('Deleted Successfully.');
+            const remaining = allReportes.filter(odr => odr._id !== id);
+            setAllReportes(remaining);
+            fetch(`http://localhost:5000/product/${proId}`, {
+                method: 'DELETE',
+            })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                  console.log(data);
+                }
+            })
+            .catch((error)=> console.log(error));
+          }
+      })
+      .catch((error)=> console.log(error));
+    }
+   }
+
   return (
     <div className='admin-all-reportes'>
+      {allReportes?.length === 0 ? <p className='text-center pt-5'>No Seller is available!</p> :
+      <>
       <TableHead table={['No.', 'Reported Product', 'Reported User', 'Report Message', 'Action']}/>
-      <SingleAdminReporte/>
+        {loading ? <Loading/> : allReportes.map((report, index) => <SingleAdminReporte key={report._id} report={report} number={index} deleteReportedHandler={deleteReportedHandler} />)}
+      </>
+      }
     </div>
   )
 }
--- src/components/dashboard/SingleAdminReporte.js
+++ src/components/dashboard/SingleAdminReporte.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { CiTrash } from 'react-icons/ci';
 import ItemTemplate from './ItemTemplate';
-import Verified from '../../assets/verified.png';
 
-function SingleAdminReporte() {
+function SingleAdminReporte({report, number, deleteReportedHandler}) {
+   const {_id, user_info, product_info, message } = report;
+
   return (
    <ItemTemplate>
       <div className='product-number' style={{width:' 100px'}}>
-         <p>1</p>
+         <p>{number+1}</p>
       </div>
       <div className='product-info'>
-         <h2>SAMSUNG Galaxy S22 Ultra</h2>
-         <h4>Price: <del>1199.99</del> <span>349.38</span></h4>
+         <h2>{product_info.title}</h2>
+         <h4>Price: <del>{product_info.original_price}</del> <span>{product_info.resale_price}</span></h4>
       </div>
       <div className='product-seller'>
-         <p>Rakib Hasan <span><img src={Verified} alt="Verified" /></span></p>
-         <p>Call: 01812345698</p>
-         <p>Area: <span>Dhaka</span></p>
+         <p>{user_info.name}</p>
+         <p>{user_info.email}</p>
       </div>
       <div className='product-info'>
-         <p>128GB, 8K Camera & Video, Brightest Display Screen...</p>
+         <p>{message}</p>
       </div>
       <div className='product-action'>
-         <button className="phoneMarket-btn"><CiTrash/></button>
+         <button className="phoneMarket-btn" onClick={()=> deleteReportedHandler(_id, product_info._id)}><CiTrash/></button>
       </div>
    </ItemTemplate>
   )
